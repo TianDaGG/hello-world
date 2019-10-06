cd362dc719fa9aecd94e0a8556dfe99ccec7fa47
TianDaGG <1105403369@qq.com>
2019-10-06 08:15:35 +0800

    Signed-off-by: TianDaGG <1105403369@qq.com>

---
+++ CMySack.cpp
@@ -0,0 +1,181 @@
+/***************************************
+*深度优先搜索实现迷宫寻路 main.cpp
+**************************************/
+#include<iostream>   
+#include<Windows.h>   
+#include"CMyStack.h"       
+#define ROW  25	//地图行数 Y       
+#define COL  25	//地图列数 X   
+#define SPEED 1	//刷新速度         
+
+struct MyPoint{
+	int row;
+	int col;
+};
+enum MyDir{ UP, DOWN, LEFT, RIGHT };	//表示方向的枚举   
+struct MyPath{
+	int  val;
+	MyDir dir;
+	bool isFind;
+};
+void drawMap(int map[ROW][COL], MyPoint point){
+	system("cls");
+	for (int i = 0; i < ROW; i++){
+		for (int j = 0; j < COL; j++){
+			if (map[i][j] == 1){//墙   
+				std::cout << "□";
+			}
+			else if (point.row == i && point.col == j)
+			{//人   
+				std::wcout << "我";
+			}
+			else{//路   
+				std::wcout << "  ";
+			}
+		}
+		std::cout << std::endl;
+	}
+	Sleep(SPEED);
+}
+int main()
+{
+	//地图 0:路 1:墙   
+	int map[ROW][COL] = {
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1 },
+		{ 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1 },
+		{ 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1 },
+		{ 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1 },
+		{ 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1 },
+		{ 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1 },
+		{ 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1 },
+		{ 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1 },
+		{ 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1 },
+		{ 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1 },
+		{ 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1 },
+		{ 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1 },
+		{ 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1 },
+		{ 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1 },
+		{ 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1 },
+		{ 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1 },
+		{ 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1 },
+		{ 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+		{ 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1 },
+		{ 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1 },
+		{ 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1 },
+		{ 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1 },
+		{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
+	};
+	//画一个二维标记数组   
+	MyPath pathMap[ROW][COL];
+	for (int i = 1; i < ROW - 1; i++)
+	{
+		for (int j = 1; j < COL - 1; j++)
+		{
+			pathMap[i][j].val = map[i][j]; //地图值   
+			pathMap[i][j].dir = RIGHT;     //所有起点默认从右先开始找   
+			pathMap[i][j].isFind = false;  //标记没走过   
+		}
+	}
+	//设置起点和终点   
+	MyPoint beginPos = { 1, 1 };
+	MyPoint endPos = { 9, 1 };//1,23   
+	CMyStack<MyPoint> myStack;    \t	nmyStack.PushStack(beginPos);
+	//当前节点   
+	MyPoint currentPos;
+	MyPoint searchPos = beginPos;
+	//死循环一个节点一个节点走动   
+	while (1){
+		//找到终点   
+		if (searchPos.row == endPos.row &&searchPos.col == endPos.col)
+		{
+			std::cout << "找到终点啦!
+				";   
+				break;
+		}
+		currentPos = searchPos;
+		switch (pathMap[searchPos.row][searchPos.col].dir)
+		{
+		case RIGHT:
+			currentPos.col++;
+			pathMap[searchPos.row][searchPos.col].dir = DOWN;
+			if (pathMap[currentPos.row][currentPos.col].val == 0 &&
+				pathMap[currentPos.row][currentPos.col].isFind == false)
+			{
+				\t	npathMap[currentPos.row][currentPos.col].isFind = true;    \t	nmyStack.PushStack(currentPos);
+				searchPos = currentPos;
+			}
+			break;
+		case DOWN:
+			currentPos.row++;
+			pathMap[searchPos.row][searchPos.col].dir = LEFT;
+			if (pathMap[currentPos.row][currentPos.col].val == 0 &&
+				pathMap[currentPos.row][currentPos.col].isFind == false)
+			{
+				pathMap[currentPos.row][currentPos.col].isFind = true;
+				myStack.PushStack(currentPos);
+				searchPos = currentPos;
+			}
+			break;
+		case LEFT:
+			currentPos.col--;
+			pathMap[searchPos.row][searchPos.col].dir = UP;
+			if (pathMap[currentPos.row][currentPos.col].val == 0 &&
+				pathMap[currentPos.row][currentPos.col].isFind == false)
+			{
+				pathMap[currentPos.row][currentPos.col].isFind = true;
+				myStack.PushStack(currentPos);
+				searchPos = currentPos;
+			}
+			break;
+		case UP:
+			currentPos.row--;
+			if (pathMap[currentPos.row][currentPos.col].val == 0 &&
+				pathMap[currentPos.row][currentPos.col].isFind == false)
+			{
+				pathMap[currentPos.row][currentPos.col].isFind = true;
+				myStack.PushStack(currentPos);
+				searchPos = currentPos;
+			}
+			else
+			{
+				myStack.PopStack();
+				searchPos = myStack.GetTopStack();
+			}
+			break;
+		}
+		//没找到出口   
+		if (myStack.IsEmptyStack())
+		{
+			std::cout << "这个迷宫没有出口!
+				";//栈空->回到原点   
+				break;
+		}
+		drawMap(map, searchPos);//每循环一次刷新一次     
+	}
+	//打印路径   
+	while (!myStack.IsEmptyStack())
+	{
+		currentPos = myStack.GetTopStack();
+		char *p;
+		switch (pathMap[currentPos.row][currentPos.col].dir)
+		{
+		case UP:
+			p = "左";
+			break;
+		case DOWN:
+			p = "右";
+			break;
+		case LEFT:
+			p = "下";
+			break;
+		case RIGHT:
+			p = "上";
+			break;
+		}
+		std::cout << "(" << currentPos.row << "," << currentPos.col << "," << p << ")";
+		myStack.PopStack();
+	}
+	return 0;
+}
---
+++ CMyStack.h
@@ -0,0 +1,72 @@
+/***********************************    
+*单链表实现栈的基本功能 CMyStack.h    
+**********************************/    
+#pragma once    
+template<typename T>    
+class CMyStack    
+{    
+public:    
+CMyStack();    
+~CMyStack();    
+void PushStack(const T& kData);    
+void PopStack();    
+T    GetTopStack();    
+bool IsEmptyStack();    
+private:    
+struct MyNode    
+{    
+		T         m_Data;    
+		MyNode *  m_pNext;    
+};    
+MyNode* m_pHead;    
+};    
+template<typename T>    
+CMyStack<T>::CMyStack()    
+{    
+		m_pHead = new MyNode;    
+		//m_pHead->m_Data = 0;    
+		m_pHead->m_pNext = nullptr;    
+}    
+template<typename T>    
+CMyStack<T>::~CMyStack()    
+{    
+		MyNode* p = m_pHead->m_pNext,*q;    
+		while (NULL != p)    
+		{    
+				q = p->m_pNext;    
+				delete p;    
+				p = q;    
+		}    
+		delete m_pHead;    
+		m_pHead->m_pNext = NULL;    
+}    
+template<typename T>    
+void CMyStack<T>::PushStack(const T& kData)    
+{    
+		MyNode* temp = new MyNode;    
+		temp->m_Data = kData;    
+		temp->m_pNext = NULL;     
+		temp->m_pNext = m_pHead->m_pNext;    
+		m_pHead->m_pNext = temp;    
+}    
+template<typename T>    
+void CMyStack<T>::PopStack()    
+{    
+		MyNode* p = m_pHead->m_pNext;    
+		MyNode* temp = p->m_pNext;    
+		delete p;    
+		p = temp;    
+		m_pHead->m_pNext = p;    
+}    
+template<typename T>    
+T CMyStack<T>::GetTopStack()    
+{    
+		MyNode* p = m_pHead->m_pNext;    
+		return p->m_Data;    
+}    
+template<typename T>//链表中没有元素就为空    
+bool CMyStack<T>::IsEmptyStack()    
+{    
+		return m_pHead->m_pNext == nullptr;    
+		//如果是nullptr返回true    
+}    
