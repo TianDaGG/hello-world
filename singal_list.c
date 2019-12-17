408847b1a4699a0038c1bb10045a1feec1cb4da1
TianDaGG <1105403369@qq.com>
2019-12-17 15:44:37 +0800

    finushed the bug of delete head node

--- singal_list.c
+++ singal_list.c
@@ -5,66 +5,66 @@
 *   Author      : mabu
 *   QQ          : 1105403369
 *   Created date: 2019-10-27 20:01:43
-*   Description : 实现简单的单链表
-*
+*   Description : 实现单向链表的增、删、查、改、排序
+*				（最后修改时间：2019-12-16 18:07:06）
 *===============================================================*/
 
-#include<iostream>
+#include<stdio.h>
 #include<string.h>
-using std::cout;
-using std::cin;
-using std::endl;
+#include<stdlib.h>
 
 typedef struct node
 {
 	int data;
 	struct node* pNext;
 }*pNode, Node;
-
-bool headIsNull = true;
+int headIsNull = 1;
 
 pNode InitNode();
 void InsertNode(pNode head, int data);
 void FindNode(pNode head, int data);
 void ModifyNode(pNode head, int oldData, int newData);
-void DeleteNode(pNode head, int delData);
+pNode DeleteNode(pNode head, int delData);
 void SortNode(pNode head);
 void FreeNode(pNode head);
 void PrintNode(pNode head);
+
 int main(int argc, char *argv[])
 {
 	pNode head = InitNode();
 	//插入
 	int listSize;
-	cout << "请输入链表大小：" << endl;
-	cin >> listSize;
+	printf("请输入链表大小：\n");
+	scanf("%d",&listSize);
 	for (int i = 0; i<listSize; ++i)
 	{
 		int num;
-		cout << "请输入要插入的数据：" << endl;
-		cin >> num;
+		printf("请输入要插入的数据：\n");
+		scanf("%d",&num);
 		InsertNode(head, num);
 	}
 	PrintNode(head);
 	//查找
 	int findNum;
-	cout << "请输入你要查找的数据：" << endl;
-	cin >> findNum;
+	printf("请输入你要查找的数据：\n");
+	scanf("%d",&findNum);
 	FindNode(head, findNum);
 	PrintNode(head);
 	//修改
 	int newData, oldData;
-	cout << "请输入你要修改的数字和修改后的数据：" << endl;
-	cin >> oldData >> newData;
+	printf("请输入你要修改的数字和修改后的数据：\n");
+	scanf("%d%d",&oldData,&newData);
 	ModifyNode(head, oldData, newData);
 	PrintNode(head);
 	//删除
 	int delData;
-	cout << "请输入要删除的数：" << endl;
-	cin >> delData;
-	DeleteNode(head, delData);
+	printf("请输入要删除的数：\n");
+	scanf("%d",&delData);
+	head=DeleteNode(head, delData);
+	int stop=0;
 	PrintNode(head);
 	//排序
+	printf("开始排序：\n");
 	SortNode(head);
 	PrintNode(head);
 	//释放所有内存
@@ -73,30 +73,28 @@ int main(int argc, char *argv[])
 }
 pNode InitNode()
 {
-	pNode tmp = new Node;
+	pNode tmp = (pNode)malloc(sizeof(Node));
 	tmp->pNext = NULL;
 	return tmp;
 }
 void InsertNode(pNode head, int data)
 {
-	if (headIsNull == true)
+	if (headIsNull == 1)
 	{
 		head->data = data;
-		headIsNull = false;
+		headIsNull = 0;
 	}
 	else
 	{
 		pNode p = head;
 		while (p->pNext != NULL)
 			p = p->pNext;
-
-		pNode temp = new node;
+		pNode temp = (pNode)malloc(sizeof(Node));
 		temp->data = data;
 		temp->pNext = NULL;
 		p->pNext = temp;
 	}
 }
-
 void FindNode(pNode head, int data)
 {
 	pNode p = head;
@@ -104,7 +102,8 @@ void FindNode(pNode head, int data)
 	{
 		if (p->data == data)
 		{
-			cout << "数据查找到了:" << p->data << endl;
+			printf("数据查找到了:%d",p->data);
+			printf("\n");
 			return;
 		}
 		else
@@ -112,12 +111,12 @@ void FindNode(pNode head, int data)
 	}
 	if (p->data == data)
 	{
-		cout << "数据查找到了:" << p->data << endl;
+		printf("数据查找到了:%d",p->data);
+		printf("\n");
 		return;
 	}
-	cout << "抱歉，没有你要查找的数据!" << endl;
+	printf("抱歉，没有你要查找的数据!\n");
 }
-
 void ModifyNode(pNode head, int oldData, int newData)
 {
 	pNode p = head;
@@ -126,7 +125,7 @@ void ModifyNode(pNode head, int oldData, int newData)
 		if (p->data == oldData)
 		{
 			p->data = newData;
-			cout << "数据修改成功!" << endl;
+			printf("数据修改成功!\n");
 			return;
 		}
 		else
@@ -135,22 +134,22 @@ void ModifyNode(pNode head, int oldData, int newData)
 	if (p->data == oldData)
 	{
 		p->data = newData;
-		cout << "数据修改成功!" << endl;
+		printf("数据修改成功!\n");
 		return;
 	}
-	cout << "抱歉，没有找到你要修改的数据！" << endl;
+	printf("抱歉，没有找到你要修改的数据！\n");
 }
-
-void DeleteNode(pNode head, int delData)
+pNode DeleteNode(pNode head, int delData)
 {
 	pNode p = head;
+	//判断当前要删除的数据是不是头结点
 	if (delData == p->data)
 	{
 		head = p->pNext;
-		delete p;
+		free(p);
 		p = NULL;
-		cout << "删除数据成功！" << endl;
-		return;
+		printf("删除数据成功！\n");
+		return head;
 	}
 	while (p->pNext)
 	{
@@ -158,20 +157,21 @@ void DeleteNode(pNode head, int delData)
 		{
 			pNode tmp = p->pNext;
 			p->pNext = tmp->pNext;
-			delete tmp;
+			free(tmp);
 			tmp = NULL;
-			cout << "数据删除成功！" << endl;
-			return;
+			printf("数据删除成功！\n");
+			return head;
 		}
 		else
 			p = p->pNext;
 	}
-	cout << "抱歉，没有找到你要删除的数据！" << endl;
+	printf("抱歉，没有找到你要删除的数据！\n");
+	return head;
 }
-
 void SortNode(pNode head)
 {
 	pNode p = head;
+	pNode tmp=NULL,q=NULL;
 	int position = 0;
 	while (p->pNext)
 	{
@@ -180,32 +180,32 @@ void SortNode(pNode head)
 	}
 	++position;
 	p = head;
-	
 	for (int i = 0; i<position - 1; ++i)
 	{
-		pNode tmp = p->pNext;
+		tmp = p->pNext;
 		for (int j = i + 1; j<position; ++j)
 		{
 			if (p->data>tmp->data)
-			{
-				pNode q = new Node;
+			{ 
+			 	q = (pNode)malloc(sizeof(Node));
 				q->data = p->data;
 				p->data = tmp->data;
 				tmp->data = q->data;
+				free(q);
+				q=NULL;
 			}
 			tmp = tmp->pNext;
 		}
 		p = p->pNext;
 	}
 }
-
 void FreeNode(pNode head)
 {
 	pNode p=head;
 	while(p)
 	{
 		pNode temp=p->pNext;
-		delete p;
+		free(p);
 		p=temp;
 	}
 }
@@ -214,8 +214,9 @@ void PrintNode(pNode head)
 	pNode p = head;
 	while (p->pNext)
 	{
-		cout << p->data << "->";
+		printf("%d->",p->data);
 		p = p->pNext;
 	}
-	cout << p->data << endl;
+	printf("%d",p->data);
+	printf("\n");
 }
